48373bff825256e79b7b49dba96ea6ffa05f03aa
wulei <1360226905@qq.com>
2016-05-12 20:13:40 +0800

    osfs09_h_DeleteFile

---
+++ osfs09/f/test.sh
@@ -0,0 +1,6 @@
+xxd -u -a -g l -c 16 -s 0x202800 -l 512 80m.img
+xxd -u -a -g l -c 16 -s 0x202A00 -l 512 80m.img
+xxd -u -a -g l -c 16 -s 0x202C00 -l 512 80m.img
+xxd -u -a -g l -c 16 -s 0x202E00 -l 512 80m.img
+xxd -u -a -g l -c 16 -s 0x203000 -l 512 80m.img
+xxd -u -a -g l -c 16 -s 0x223000 -l 512 80m.img
---
+++ osfs09/h/ReadMe.txt
@@ -0,0 +1,4 @@
+delete fs/open.c 324ln   assert(nr_sects_to_alloc == 0); 
+delete fs/link.c 123ln   assert((fsbuf[byte_idx % SECTOR_SIZE] >> i & 1) == 1);
+delete fs/link.c 136ln   assert(fsbuf[i] == 0xFF);
+delete fs/link.c 147ln   assert((fsbuf[i] & mask) == mask); 
